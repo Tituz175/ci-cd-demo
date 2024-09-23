e78b589baae6691c7c4b7c127efcff50563fa2e0
Tobi Oyekanmi <71074506+Tituz175@users.noreply.github.com>
2024-09-22 21:00:26 -0600

    CI
    The main.py file was created alongside the test file and the CI actions

---
+++ main.py
@@ -0,0 +1,8 @@
+class Calculator():
+    
+  def add(self, a, b):
+      return a + b
+
+if __name__ == "__main__":
+    mycal = Calculator()
+    print(mycal.add(5, 10))  # Expected output: 15
---
+++ test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main import Calculator
+
+c = Calculator()
+
+class TestMain(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(c.add(3, 7), 10)
+        self.assertEqual(c.add(-1, 1), 0)
+
+if __name__ == "__main__":
+    unittest.main()
